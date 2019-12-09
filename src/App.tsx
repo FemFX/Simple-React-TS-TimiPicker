a4a9665c105e9ade3a0e8a122a3d5c32c03377b2
FemFX <ignedko34@gmail.com>
2019-12-09 17:10:45 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import Time from './components/Time';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Time />
+    </>
+  );
+};
+
+export default App;
